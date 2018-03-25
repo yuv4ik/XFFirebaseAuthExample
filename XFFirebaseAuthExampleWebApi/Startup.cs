7431713699b5afc75876938d44244e71118d7212
Evgeny Zborovsky <yuv4ik@gmail.com>
2018-03-25 22:34:36 +0300

    Update Startup.cs

--- XFFirebaseAuthExampleWebApi/Startup.cs
+++ XFFirebaseAuthExampleWebApi/Startup.cs
@@ -35,7 +35,6 @@ namespace XFFirebaseAuthExampleWebApi
                 };
             });
             services.AddMvc();
-            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
